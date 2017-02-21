741ae26ba6148f9ce7fa9d919fe7823eac624747
Cole McMullin <323928036@C-DNY5WR1.tdsb.on.ca>
2017-02-21 14:50:32 -0500

    added Goodbye cruel world

--- Hello/src/mcMullin/HelloWorld.java
+++ Hello/src/mcMullin/HelloWorld.java
@@ -8,8 +8,8 @@ package mcMullin;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
 			System.out.println("Hello,World!");
+			System.out.println("Goodbye cruel world");
 	}
 
 }
