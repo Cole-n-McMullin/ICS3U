83a424a4719f8342f47739e817b744fc5368452f
Cole McMullin <323928036@C-DNY5WR1.tdsb.on.ca>
2017-02-21 14:44:31 -0500

    pushed Hello World

---
+++ Hello/src/mcMullin/HelloWorld.java
@@ -0,0 +1,15 @@
+package mcMullin;
+/**
+ * HelloWorld.Java
+ * prints Hello, World! to the Console
+ * @author Cole McMullin
+ *Tues.,Feb.21,2017
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+			System.out.println("Hello,World!");
+	}
+
+}
