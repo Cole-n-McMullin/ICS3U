d72f78d92345e67179056f36f4111a1b900aeb6b
colemcmullin <zycabo@gmail.com>
2017-04-13 14:14:35 -0400

    added isPalendrome.java

--- Unit2/src/mcmullin/IsPalendrome.java
+++ Unit2/src/mcmullin/IsPalendrome.java
@@ -1,7 +1,12 @@
 package mcmullin;
 
 import java.util.Scanner;
-
+/**
+ * isPalendrome.java
+ * april 13 2017
+ * @author Cole McMullin
+ *gets a string from the user and tells them wether or not it is a palendrome
+ */
 public class IsPalendrome {
 	
 	static Scanner in = new Scanner(System.in);
@@ -10,6 +15,11 @@ public class IsPalendrome {
 		String input = in.nextLine();
 		System.out.println(isPalendrome(input));
 	}
+	/**
+	 * 
+	 * @param s the input string
+	 * @return weather or not the word is a palendrome
+	 */
 	public static boolean isPalendrome(String s){
 		char[] letters = s.toCharArray();
 		String inverse = "";
